e26ad2f1cd1486af8ebb775ea2525f66451d7c90
gn64_jp <gn64@rec10.org>
2014-10-30 17:29:52 +0900

    remove python resource.

--- libre10.rb
+++ libre10.rb
@@ -20,36 +20,41 @@ class Libre10 < Formula
   depends_on "poppler" 
   depends_on "libjpeg-turbo"
   depends_on :java => "1.7"
+  depends_on 'pycrypto' => [:python,"Crypto"]
+  depends_on 'requests' => :python
+  depends_on 'pillow' => [:python,"PIL"]
+  depends_on 'docopt' => :python
 
-  resource "pycrypto" do
-    url "https://pypi.python.org/packages/source/p/pycrypto/pycrypto-2.6.tar.gz"
-    sha1 "c17e41a80b3fbf2ee4e8f2d8bb9e28c5d08bbb84"
-  end
+  #resource "pycrypto" do
+  #  url "https://pypi.python.org/packages/source/p/pycrypto/pycrypto-2.6.tar.gz"
+  #  sha1 "c17e41a80b3fbf2ee4e8f2d8bb9e28c5d08bbb84"
+  #end
 
-  resource "requests" do
-    url "https://pypi.python.org/packages/source/r/requests/requests-2.4.3.tar.gz"
-    sha1 ""
-  end
+  #resource "requests" do
+  #  url "https://pypi.python.org/packages/source/r/requests/requests-2.4.3.tar.gz"
+  #  sha1 ""
+  #end
 
-  resource "pillow" do
-    url "https://pypi.python.org/packages/source/P/Pillow/Pillow-2.6.1.tar.gz"
-    sha1 ""
-  end
+  #resource "pillow" do
+  #  url "https://pypi.python.org/packages/source/P/Pillow/Pillow-2.6.1.tar.gz"
+  #  sha1 ""
+  #end
 
-  resource "docopt" do
-    url "https://pypi.python.org/packages/source/d/docopt/docopt-0.6.2.tar.gz"
-    sha1 ""
+  #resource "docopt" do
+  #  url "https://pypi.python.org/packages/source/d/docopt/docopt-0.6.2.tar.gz"
+  #  sha1 ""
+  #end
 
   def install
-    resource("pycrypto").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
-    resource("prequests").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
-    resource("pillow").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
-    resource("docopt").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
+    #resource("pycrypto").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
+    #resource("prequests").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
+    #resource("pillow").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
+    #resource("docopt").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
     system "python ./www/libre10_exec.py install --data-dir=#{share} --bin-dir=#{bin} --www-dir=#{prefix}/www"
 
   end
 
-  #test do
+  test do
     # `test do` will create, run in and delete a temporary directory.
     #
     # This test will fail and we won't accept that! It's enough to just replace
@@ -59,6 +64,6 @@ class Libre10 < Formula
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
-  #  system "false"
-  #end
+    system "true"
+  end
 end
