cab04b1d8fa3d75fa5c346553c42bb989b83c7e8
gn64_jp <gn64@rec10.org>
2014-11-02 23:40:40 +0900

    add python resource files.

--- libre10.rb
+++ libre10.rb
@@ -19,14 +19,47 @@ class Libre10 < Formula
   depends_on "poppler" 
   depends_on "libjpeg-turbo"
   depends_on :java => "1.7"
-  depends_on 'pycrypto' => [:python,"Crypto"]
-  depends_on 'requests' => :python
-  depends_on 'pillow' => [:python,"PIL"]
-  depends_on 'docopt' => :python
+  depends_on :python
+  #depends_on 'pycrypto' => [:python,"Crypto"]
+  #depends_on 'requests' => :python
+  #depends_on 'pillow' => [:python,"PIL"]
+  #depends_on 'docopt' => :python
 
+  resource "pycrypto" do
+    url "https://pypi.python.org/packages/source/p/pycrypto/pycrypto-2.6.1.tar.gz"
+    sha1 ""
+  end
+  
+  resource "requests" do
+    url "https://pypi.python.org/packages/source/r/requests/requests-2.4.3.tar.gz"
+    sha1 ""
+  end
+  
+  resource "pillow" do
+    url "https://pypi.python.org/packages/source/P/Pillow/Pillow-2.6.1.tar.gz"
+    sha1 ""
+  end
+  
+  resource "docopt" do
+    url "https://pypi.python.org/packages/source/d/docopt/docopt-0.6.2.tar.gz"
+    sha1 ""
+  end
+  
+  resource "cherrypy" do
+    url "https://pypi.python.org/packages/source/C/CherryPy/CherryPy-3.6.0.tar.gz"
+    sha ""
+  end
+    
   def install
+    resource("pycrypto").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
+    resource("prequests").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
+    resource("pillow").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
+    resource("docopt").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
+    resource("cherrypy").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
     inreplace "conf/org.rec10.libre10.solr.plist", "[username]", `whoami`.gsub("\n","")
+    inreplace "conf/org.rec10.libre10.wsgi.plist", "[username]", `whoami`.gsub("\n","")
     system "cp conf/org.rec10.libre10.solr.plist ~/Library/LaunchAgents"
+    system "cp conf/org.rec10.libre10.wsgi.plist ~/Library/LaunchAgents"
     system "python ./www/libre10_exec.py install --data-dir=#{HOMEBREW_PREFIX}/var --bin-dir=#{bin} --www-dir=#{prefix}/www"
 
   end
