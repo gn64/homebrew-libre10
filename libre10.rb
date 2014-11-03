1bf26ba71b642bc4d900df4503083d23db005eee
gn64_jp <gn64@rec10.org>
2014-11-03 15:59:17 +0900

    change head branch.

--- libre10.rb
+++ libre10.rb
@@ -10,7 +10,7 @@ class Libre10 < Formula
   url 'https://bitbucket.org/gn64/libre10.git', :tag => "#{HOMEBREW_BREWALL_VERSION}"
   sha1 ""
   version HOMEBREW_BREWALL_VERSION
-  head 'https://bitbucket.org/gn64/libre10.git', :branch => 'release/1.1.0'
+  head 'https://bitbucket.org/gn64/libre10.git', :branch => 'master'
   depends_on "libtiff" 
   depends_on "libjpeg"
   depends_on "webp" 
@@ -27,41 +27,44 @@ class Libre10 < Formula
 
   resource "pycrypto" do
     url "https://pypi.python.org/packages/source/p/pycrypto/pycrypto-2.6.1.tar.gz"
-    sha1 ""
+    sha1 "aeda3ed41caf1766409d4efc689b9ca30ad6aeb2"
   end
   
   resource "requests" do
     url "https://pypi.python.org/packages/source/r/requests/requests-2.4.3.tar.gz"
-    sha1 ""
+    sha1 "411f1bfa44556f7dd0f34cd822047c31baa7d741"
   end
   
   resource "pillow" do
     url "https://pypi.python.org/packages/source/P/Pillow/Pillow-2.6.1.tar.gz"
-    sha1 ""
+    sha1 "d789cad38db37b14d7650817111f000da7776c73"
   end
   
   resource "docopt" do
     url "https://pypi.python.org/packages/source/d/docopt/docopt-0.6.2.tar.gz"
-    sha1 ""
+    sha1 "224a3ec08b56445a1bd1583aad06b00692671e04"
   end
   
   resource "cherrypy" do
     url "https://pypi.python.org/packages/source/C/CherryPy/CherryPy-3.6.0.tar.gz"
-    sha ""
+    sha1 "bbbeb4e2bb81cb37049a32f78c6cb151b12d7857"
   end
     
   def install
-    resource("pycrypto").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
-    resource("prequests").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
+    ENV["PYTHONPATH"] = lib+"python2.7/site-packages"
+    ENV.prepend_create_path "PYTHONPATH", libexec+"lib/python2.7/site-packages"
+    ENV.prepend_create_path "PYTHONPATH", prefix+"lib/python2.7/site-packages"
     resource("pillow").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
     resource("docopt").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
-    resource("cherrypy").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
+    #resource("cherrypy").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
+    resource("pycrypto").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
+    resource("requests").stage { system "python", "setup.py", "install", "--prefix=#{libexec}" }
     inreplace "conf/org.rec10.libre10.solr.plist", "[username]", `whoami`.gsub("\n","")
     inreplace "conf/org.rec10.libre10.wsgi.plist", "[username]", `whoami`.gsub("\n","")
-    system "cp conf/org.rec10.libre10.solr.plist ~/Library/LaunchAgents"
-    system "cp conf/org.rec10.libre10.wsgi.plist ~/Library/LaunchAgents"
+    #system "cp conf/org.rec10.libre10.solr.plist ~/Library/LaunchAgents"
+    #system "cp conf/org.rec10.libre10.wsgi.plist ~/Library/LaunchAgents"
     system "python ./www/libre10_exec.py install --data-dir=#{HOMEBREW_PREFIX}/var --bin-dir=#{bin} --www-dir=#{prefix}/www"
-
+    bin.env_script_all_files(libexec+"bin", :PYTHONPATH => ENV["PYTHONPATH"])
   end
 
   test do
